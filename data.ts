838f56c04c9869301bf799941121e46862be4273
Ellie <sgyoon.info@gmail.com>
2021-01-18 18:35:55 +0900

    Fix fingerprint를 파일명 뒤에 물음표를 붙여 더해서 파일 덮어씌우기가 가능하도록 함

--- data.ts
+++ data.ts
@@ -11,142 +11,142 @@ export const PET_IMAGES = [
   {
     imgUrl: `cat04.jpeg`,
   },
-  // {
-  //   imgUrl: `/images/cat05.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat06.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat07.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat08.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat09.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat10.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat11.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat12.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat13.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat14.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat15.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat16.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat17.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat18.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat19.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat20.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat21.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat22.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat23.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat24.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/cat25.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog01.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog02.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog03.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog04.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog05.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog06.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog07.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog08.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog09.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog10.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog11.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog12.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog13.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog14.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog15.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog16.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog17.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog18.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog19.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog20.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog21.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog22.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog23.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog24.jpeg`,
-  // },
-  // {
-  //   imgUrl: `/images/dog25.jpeg`,
-  // },
+  {
+    imgUrl: `cat05.jpeg`,
+  },
+  {
+    imgUrl: `cat06.jpeg`,
+  },
+  {
+    imgUrl: `cat07.jpeg`,
+  },
+  {
+    imgUrl: `cat08.jpeg`,
+  },
+  {
+    imgUrl: `cat09.jpeg`,
+  },
+  {
+    imgUrl: `cat10.jpeg`,
+  },
+  {
+    imgUrl: `cat11.jpeg`,
+  },
+  {
+    imgUrl: `cat12.jpeg`,
+  },
+  {
+    imgUrl: `cat13.jpeg`,
+  },
+  {
+    imgUrl: `cat14.jpeg`,
+  },
+  {
+    imgUrl: `cat15.jpeg`,
+  },
+  {
+    imgUrl: `cat16.jpeg`,
+  },
+  {
+    imgUrl: `cat17.jpeg`,
+  },
+  {
+    imgUrl: `cat18.jpeg`,
+  },
+  {
+    imgUrl: `cat19.jpeg`,
+  },
+  {
+    imgUrl: `cat20.jpeg`,
+  },
+  {
+    imgUrl: `cat21.jpeg`,
+  },
+  {
+    imgUrl: `cat22.jpeg`,
+  },
+  {
+    imgUrl: `cat23.jpeg`,
+  },
+  {
+    imgUrl: `cat24.jpeg`,
+  },
+  {
+    imgUrl: `cat25.jpeg`,
+  },
+  {
+    imgUrl: `dog01.jpeg`,
+  },
+  {
+    imgUrl: `dog02.jpeg`,
+  },
+  {
+    imgUrl: `dog03.jpeg`,
+  },
+  {
+    imgUrl: `dog04.jpeg`,
+  },
+  {
+    imgUrl: `dog05.jpeg`,
+  },
+  {
+    imgUrl: `dog06.jpeg`,
+  },
+  {
+    imgUrl: `dog07.jpeg`,
+  },
+  {
+    imgUrl: `dog08.jpeg`,
+  },
+  {
+    imgUrl: `dog09.jpeg`,
+  },
+  {
+    imgUrl: `dog10.jpeg`,
+  },
+  {
+    imgUrl: `dog11.jpeg`,
+  },
+  {
+    imgUrl: `dog12.jpeg`,
+  },
+  {
+    imgUrl: `dog13.jpeg`,
+  },
+  {
+    imgUrl: `dog14.jpeg`,
+  },
+  {
+    imgUrl: `dog15.jpeg`,
+  },
+  {
+    imgUrl: `dog16.jpeg`,
+  },
+  {
+    imgUrl: `dog17.jpeg`,
+  },
+  {
+    imgUrl: `dog18.jpeg`,
+  },
+  {
+    imgUrl: `dog19.jpeg`,
+  },
+  {
+    imgUrl: `dog20.jpeg`,
+  },
+  {
+    imgUrl: `dog21.jpeg`,
+  },
+  {
+    imgUrl: `dog22.jpeg`,
+  },
+  {
+    imgUrl: `dog23.jpeg`,
+  },
+  {
+    imgUrl: `dog24.jpeg`,
+  },
+  {
+    imgUrl: `dog25.jpeg`,
+  },
 ];
--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ module.exports = {
   webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
     // file-loader는 프로젝트에 사용되는 파일들을 모두 복사한다. public 안에 있는 파일도 모두.
     // 서버와 브라우저 각각 한번씩 실행되므로 복사된 파일은 각각의 폴더에 존재하게 된다.
-    
+
     // 빌드하거나 배포해보면 해당 파일들은 웹상에서는 보이지 않는다. static 폴더만 보인다.
-    // 브라우저에서는 파일이 복사되지 않게 해보려 했으나 실패했다. 파일을 복사하는게 파일 로더의 일이므로..
-    // 브라우저에서 파일 로더 자체를 돌지 않게 하면 이미지를 required로 가져온 부분에서 파싱 에러가 난다.
+    // 브라우저 혹은 서버측 하나에서만 파일 로더를 돌게 하면 이미지를 required로 가져온 부분에서 파싱 에러가 난다.
 
     // Module parse failed: Unexpected character '�' (1:0)
     // You may need an appropriate loader to handle this file type,
@@ -13,38 +12,27 @@ module.exports = {
     // (Source code omitted for this binary file)
 
     // 서버에 생기는 파일들은 완전히 지워지고 다시 생겨서, 예전에 생성된 파일이 남아있지 않다.
-    // 하지만 public/hashedImg에는 예전에 생성된, 바뀌기 전 파일도 남아있다. 사실 이 폴더는 없어도 상관이 없다.
+    // 하지만 public/hashedImg에는 예전에 생성된, 바뀌기 전 파일도 남아있다. 이걸 지울 수 없을까?
+    // -> 파일 뒤에 ?를 붙이고 해싱을 붙이면, 파일 자체는 다른 파일로 인식되면서도 파일명은 같은 파일명으로 가져갈 수 있다.
+
     // 만약 브라우저의 public/images 폴더에 그냥 빌드한 이미지를 넣으면, dev에서는 파일의 변화가 있다고 감지하고 무한 핫로딩에 빠진다.
+    // 그래서 프로덕션에서만 같은 폴더에 파일이 들어가도록(덮어씌워지도록) 설정함
+    // 사실 이게 어떤 의미가 있는지는 정확하게 모르겠다.
 
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
       use: {
         loader: 'file-loader',
         options: {
-          // 이름.해시(파일이 변경된 경우에만 변경됨).확장자 형태로 파일 이름을 설정한다
-          name: isServer ? '[name].[contenthash].[ext]' : '[name].[ext]',
+          // '[이름].[확장자]?[해시(파일이 변경된 경우에만 변경됨)] 형태로 파일 이름을 설정한다
+          name: '[name].[ext]?[contenthash]',
           // 빌드된 파일이 저장될 경로
-          outputPath: !dev ? '../public/images/' : '../public/hashedImages/',
+          outputPath: dev ? '../public/hashedImages/' : '../public/images/',
           // 경로 앞에 붙을 경로. 설정하지 않으면 _next/부터 시작한다.
-          publicPath: !dev ? '/images/' : '/hashedImages/',
+          publicPath: dev ? '/hashedImages/' : '/images/',
         },
       },
     });
-
-    // config.module.rules.push({
-    //   test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
-    //   use: {
-    //     loader: 'file-loader',
-    //     options: {
-    //       // 이름.해시(파일이 변경된 경우에만 변경됨).확장자 형태로 파일 이름을 설정한다
-    //       name: '[name].[contenthash].[ext]',
-    //       // 빌드된 파일이 저장될 경로
-    //       outputPath: '../public/hashedImages/',
-    //       // 경로 앞에 붙을 경로. 설정하지 않으면 _next/부터 시작한다.
-    //       publicPath: '/hashedImages/',
-    //     },
-    //   },
-    // });
 
     return config;
   },
