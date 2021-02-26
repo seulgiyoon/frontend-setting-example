d23cfd1d7564fbc8eb8e7339fec666a6fc217d84
Ellie <sgyoon.info@gmail.com>
2021-02-26 18:21:25 +0900

    Fix 릴리즈 빌드 설정 수정

--- website/next.config.js
+++ website/next.config.js
@@ -45,6 +45,9 @@ module.exports = withSourceMaps(
             ignore: ['node_modules'],
             urlPrefix: '~/_next',
             release: COMMIT_SHA,
+            deploy: {
+              env: 'production',
+            },
           }),
         );
       }
