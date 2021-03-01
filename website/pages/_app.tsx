fd70ef728ae0d55d35db2d1de0521769102f9a0f
Ellie <sgyoon.info@gmail.com>
2021-03-02 01:11:25 +0900

    Fix app 파일 설정변경

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -8,8 +8,28 @@ import { wrapper } from '../src/global/store/store';
 // init();
 
 import * as Sentry from '@sentry/node';
+import { RewriteFrames } from '@sentry/integrations';
 
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  const integrations = [];
+  if (
+    process.env.NEXT_IS_SERVER === 'true' &&
+    process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
+  ) {
+    integrations.push(
+      new RewriteFrames({
+        iteratee: (frame) => {
+          frame.filename = frame.filename.replace(
+            process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
+            'app:///',
+          );
+          frame.filename = frame.filename.replace('.next', '_next');
+          return frame;
+        },
+      }),
+    );
+  }
+
   Sentry.init({
     enabled: process.env.NODE_ENV === 'production',
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
