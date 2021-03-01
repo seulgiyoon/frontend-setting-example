96e0f64b69ac1a40939479377a9a13c8d504fbe4
Ellie <sgyoon.info@gmail.com>
2021-03-01 21:07:34 +0900

    Fix 빌드 설정

--- website/next.config.js
+++ website/next.config.js
@@ -2,7 +2,14 @@ const withTM = require('next-transpile-modules')(['@monorepo/ui']);
 const withSourceMaps = require('@zeit/next-source-maps')({
   devtool: 'source-map',
 });
-
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+} = process.env;
 const basePath = '';
 
 module.exports = withSourceMaps(
@@ -10,6 +17,23 @@ module.exports = withSourceMaps(
     webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
       if (!isServer) {
         config.resolve.alias['@sentry/node'] = '@sentry/browser';
+      }
+
+      if (
+        SENTRY_DSN &&
+        SENTRY_ORG &&
+        SENTRY_PROJECT &&
+        SENTRY_AUTH_TOKEN &&
+        NODE_ENV === 'production'
+      ) {
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            include: '.next',
+            ignore: ['node_modules'],
+            urlPrefix: '~/_next',
+            release: option,
+          }),
+        );
       }
 
       config.module.rules.push({
@@ -33,7 +57,6 @@ module.exports = withSourceMaps(
       return config;
     },
     basePath,
-    productionBrowserSourceMaps: true,
     images: {
       domains: ['via.placeholder.com'],
     },
